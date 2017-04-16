546294a80e7211eea36ecc630bc628428447a7e7
Clay Kohut <claykohut@MacBook-Pro-3.local>
2017-04-16 18:34:40 -0400

    setting up deck drag

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react'
-import { View, Animated } from 'react-native'
+import {
+  View,
+  Animated,
+  PanResponder
+} from 'react-native'
 
 class Deck extends Component {
+
+  constructor(props){
+    super(props)
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx , y: gesture.dy })
+      },
+      onPanResponderRelease: () => {
+
+      }
+    })
+
+    this.state = {
+      panResponder,
+      position
+    }
+  }
 
   renderCards = () => {
       const { data, renderCard } = this.props
@@ -12,9 +37,12 @@ class Deck extends Component {
 
   render(){
     return (
-      <View>
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     )
   }
 }
